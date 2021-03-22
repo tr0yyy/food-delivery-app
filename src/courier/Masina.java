595c078795deb802cc3b2a7ba89abe71d1ed3458
Alexandru Nicoi <63098825+tr0yyy@users.noreply.github.com>
2021-03-22 11:14:15 +0200

    immutable class Masina

--- src/courier/Masina.java
+++ src/courier/Masina.java
@@ -1,9 +1,9 @@
 package courier;
 
-public class Masina {
-    private String marca;
-    private String model;
-    private String nr_inmatriculare;
+public final class Masina {
+    private final String marca;
+    private final String model;
+    private final String nr_inmatriculare;
 
     public Masina(String marca, String model, String nr_inmatriculare) {
         this.marca = marca;
@@ -15,24 +15,12 @@ public class Masina {
         return marca;
     }
 
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
     public String getModel() {
         return model;
     }
 
-    public void setModel(String model) {
-        this.model = model;
-    }
-
     public String getNr_inmatriculare() {
         return nr_inmatriculare;
-    }
-
-    public void setNr_inmatriculare(String nr_inmatriculare) {
-        this.nr_inmatriculare = nr_inmatriculare;
     }
 
     @Override
